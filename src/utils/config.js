7fdcf0fbef126e5093c004eb579330014e00531b
Rbazan22 <rbazan22@stu.southtexascollege.edu>
2022-03-25 18:47:28 -0500

    Added logger

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 import joi from 'joi'
 
+import logger from './logger'
+
 dotenv.config()
 
 const envSchema = joi
@@ -12,11 +14,11 @@ const envSchema = joi
   .unknown()
 
 const { value: env, error } = envSchema
-  .preferences({ errors: { label: 'key' } })
+  .prefs({ errors: { label: 'key' } })
   .validate(process.env)
 
 if (error) {
-  throw new Error('Config validation error: $(error.message)')
+  logger.log.error(new Error('Config validation error: $(error.message)'))
 }
 
 export default { nodeEnv: env.NODE_ENV, port: env.PORT }
